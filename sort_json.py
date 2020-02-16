12fc710b2e66fadc7ba69ad021c6f570a37b3c4b
Matteo Pergolesi <matpergo@gmail.com>
2020-02-16 19:41:16 +0100

    Remove old file.

--- sort_json.py
+++
@@ -1,7 +0,0 @@
-import json
-import sys
-
-if __name__ == '__main__':
-    with open(sys.argv[1]) as json_file:
-        json_data = json.load(json_file)
-        print(json.dumps(json_data, sort_keys=True, indent=2))
