dda2363501f046cc92c5ca0ddee6ea34c8bf2d03
Matteo Pergolesi <matpergo@gmail.com>
2020-02-16 13:10:57 +0100

    Reformat line.

--- json2openapi.py
+++ json2openapi.py
@@ -82,7 +82,8 @@ def _get_parser():
     parser.add_argument("method", type=str,
                         choices=["GET", "POST", "PUT", "PATCH", "DELETE"],
                         help="HTTP request method", metavar="METHOD")
-    parser.add_argument("path", type=str, help="URI resource path", metavar="PATH")
+    parser.add_argument("path", type=str, help="URI resource path",
+                        metavar="PATH")
     parser.add_argument("resp_code", type=int, help="HTTP response code",
                         metavar="CODE")
     parser.add_argument("--req-json", "-reqj", type=str,
