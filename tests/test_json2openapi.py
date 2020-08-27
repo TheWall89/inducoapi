3ca9f82ae36047d5f546749dd817fe04a2dfad4d
Matteo Pergolesi <matpergo@gmail.com>
2020-08-27 15:05:53 +0200

    Fix pytest relative paths. Project directory is working directory.

--- tests/test_json2openapi.py
+++ tests/test_json2openapi.py
@@ -7,22 +7,22 @@ from json2openapi.json2openapi import build_openapi
 def test_get_employees_200():
   oapi = build_openapi('GET', '/employees', 200)
   OpenAPI(oapi)
-  with open('test_get_employees_200.yaml') as f:
+  with open('tests/test_get_employees_200.yaml') as f:
     assert yaml.safe_load(f.read()) == oapi
 
 
 def test_get_employees_200_response():
   oapi = build_openapi('GET', '/employees', 200,
-                       response='../examples/employees.json')
+                       response='examples/employees.json')
   OpenAPI(oapi)
-  with open('test_get_employees_200_response.yaml') as f:
+  with open('tests/test_get_employees_200_response.yaml') as f:
     assert yaml.safe_load(f.read()) == oapi
 
 
 def test_post_employees_201_request_response():
   oapi = build_openapi('POST', '/employees', 201,
-                       request='../examples/new_employee_req.json',
-                       response='../examples/new_employee_resp.json')
+                       request='examples/new_employee_req.json',
+                       response='examples/new_employee_resp.json')
   OpenAPI(oapi)
-  with open('test_post_employees_201_request_response.yaml') as f:
+  with open('tests/test_post_employees_201_request_response.yaml') as f:
     assert yaml.safe_load(f.read()) == oapi
