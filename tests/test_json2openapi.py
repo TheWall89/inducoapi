b448c8e1cd8f4aeaebd2c66a919a11e0662c5d7f
Matteo Pergolesi <matpergo@gmail.com>
2020-08-27 14:54:30 +0200

    Add test with response.

--- tests/test_json2openapi.py
+++ tests/test_json2openapi.py
@@ -9,3 +9,9 @@ def test_get_employees_200():
   OpenAPI(oapi)
   with open('test_get_employees_200.yaml') as f:
     assert yaml.safe_load(f.read()) == oapi
+
+def test_get_employees_200_response():
+  oapi = build_openapi('GET', '/employees', 200, response='../examples/employees.json')
+  OpenAPI(oapi)
+  with open('test_get_employees_200_response.yaml') as f:
+    assert yaml.safe_load(f.read()) == oapi
