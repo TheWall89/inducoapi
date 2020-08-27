70bfdf85c126ee8fa27bab9213405b04a8ad972e
Matteo Pergolesi <matpergo@gmail.com>
2020-08-27 15:02:36 +0200

    Add test for post generation with request and response.

--- tests/test_json2openapi.py
+++ tests/test_json2openapi.py
@@ -10,8 +10,19 @@ def test_get_employees_200():
   with open('test_get_employees_200.yaml') as f:
     assert yaml.safe_load(f.read()) == oapi
 
+
 def test_get_employees_200_response():
-  oapi = build_openapi('GET', '/employees', 200, response='../examples/employees.json')
+  oapi = build_openapi('GET', '/employees', 200,
+                       response='../examples/employees.json')
   OpenAPI(oapi)
   with open('test_get_employees_200_response.yaml') as f:
+    assert yaml.safe_load(f.read()) == oapi
+
+
+def test_post_employees_201_request_response():
+  oapi = build_openapi('POST', '/employees', 201,
+                       request='../examples/new_employee_req.json',
+                       response='../examples/new_employee_resp.json')
+  OpenAPI(oapi)
+  with open('test_post_employees_201_request_response.yaml') as f:
     assert yaml.safe_load(f.read()) == oapi
