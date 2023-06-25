04eed977db007eb57ad374b522672a07e479711f
Matteo Pergolesi <matpergo@gmail.com>
2023-06-25 14:58:58 +0200

    Format test file

--- tests/test_inducoapi.py
+++ tests/test_inducoapi.py
@@ -7,9 +7,7 @@ pytestmark = pytest.mark.parametrize("openapi_version", [("3.0.0"), ("3.1.0")])
 
 def test_get_employees_200(openapi_version):
     oapi = build_openapi("GET", "/employees", "200", openapi_version=openapi_version)
-    spec_dict, spec_url = read_from_filename(
-        "tests/test_get_employees_200.yaml"
-    )
+    spec_dict, spec_url = read_from_filename("tests/test_get_employees_200.yaml")
     spec_dict["openapi"] = openapi_version
     assert oapi == spec_dict
 
@@ -20,22 +18,23 @@ def test_get_employees_200_params(openapi_version):
         "/employees",
         "200",
         parameters=[("id", "path"), ("limit", "query"), ("token", "header")],
-        openapi_version=openapi_version
+        openapi_version=openapi_version,
     )
-    spec_dict, spec_url = read_from_filename(
-        "tests/test_get_employees_200_params.yaml"
-    )
+    spec_dict, spec_url = read_from_filename("tests/test_get_employees_200_params.yaml")
     spec_dict["openapi"] = openapi_version
     assert oapi == spec_dict
 
 
 def test_get_employees_200_info(openapi_version):
     oapi = build_openapi(
-        "GET", "/employees", "200", title="Custom Title", version="v1-custom", openapi_version=openapi_version
+        "GET",
+        "/employees",
+        "200",
+        title="Custom Title",
+        version="v1-custom",
+        openapi_version=openapi_version,
     )
-    spec_dict, spec_url = read_from_filename(
-        "tests/test_get_employees_200_info.yaml"
-    )
+    spec_dict, spec_url = read_from_filename("tests/test_get_employees_200_info.yaml")
     spec_dict["openapi"] = openapi_version
     assert oapi == spec_dict
 
@@ -43,7 +42,9 @@ def test_get_employees_200_info(openapi_version):
 def test_get_employees_200_response(openapi_version):
     with open("examples/employees.json") as f:
         response = f.read()
-    oapi = build_openapi("GET", "/employees", "200", response=response, openapi_version=openapi_version)
+    oapi = build_openapi(
+        "GET", "/employees", "200", response=response, openapi_version=openapi_version
+    )
     spec_dict, spec_url = read_from_filename(
         "tests/test_get_employees_200_response.yaml"
     )
@@ -55,7 +56,12 @@ def test_get_employees_200_response_ref(openapi_version):
     with open("examples/employees.json") as f:
         response = f.read()
     oapi = build_openapi(
-        "GET", "/employees", "200", response=response, reference=True, openapi_version=openapi_version
+        "GET",
+        "/employees",
+        "200",
+        response=response,
+        reference=True,
+        openapi_version=openapi_version,
     )
     spec_dict, spec_url = read_from_filename(
         "tests/test_get_employees_200_response_ref.yaml"
@@ -68,7 +74,12 @@ def test_get_employees_200_response_noexample(openapi_version):
     with open("examples/employees.json") as f:
         response = f.read()
     oapi = build_openapi(
-        "GET", "/employees", "200", response=response, example=False, openapi_version=openapi_version
+        "GET",
+        "/employees",
+        "200",
+        response=response,
+        example=False,
+        openapi_version=openapi_version,
     )
     spec_dict, spec_url = read_from_filename(
         "tests/test_get_employees_200_response_noexample.yaml"
@@ -80,7 +91,9 @@ def test_get_employees_200_response_noexample(openapi_version):
 def test_get_employees_200_response_yaml(openapi_version):
     with open("examples/employees.yaml") as f:
         response = f.read()
-    oapi = build_openapi("GET", "/employees", "200", response=response, openapi_version=openapi_version)
+    oapi = build_openapi(
+        "GET", "/employees", "200", response=response, openapi_version=openapi_version
+    )
     spec_dict, spec_url = read_from_filename(
         "tests/test_get_employees_200_response.yaml"
     )
@@ -112,7 +125,12 @@ def test_post_employees_201_request_response(openapi_version):
     with open("examples/new_employee_resp.json") as f:
         response = f.read()
     oapi = build_openapi(
-        "POST", "/employees", "201", request=request, response=response, openapi_version=openapi_version
+        "POST",
+        "/employees",
+        "201",
+        request=request,
+        response=response,
+        openapi_version=openapi_version,
     )
     spec_dict, spec_url = read_from_filename(
         "tests/test_post_employees_201_request_response.yaml"
@@ -125,7 +143,13 @@ def test_post_employees_201_request_invalid_json(openapi_version):
     with open("tests/invalid.json") as f:
         request = f.read()
     with pytest.raises(ValueError) as excinfo:
-        build_openapi("POST", "/employees", "201", request=request, openapi_version=openapi_version)
+        build_openapi(
+            "POST",
+            "/employees",
+            "201",
+            request=request,
+            openapi_version=openapi_version,
+        )
     assert "request" in str(excinfo)
 
 
@@ -133,7 +157,13 @@ def test_post_employees_201_request_invalid_yaml(openapi_version):
     with open("tests/invalid.yaml") as f:
         request = f.read()
     with pytest.raises(ValueError) as excinfo:
-        build_openapi("POST", "/employees", "201", request=request, openapi_version=openapi_version)
+        build_openapi(
+            "POST",
+            "/employees",
+            "201",
+            request=request,
+            openapi_version=openapi_version,
+        )
     assert "request" in str(excinfo)
 
 
@@ -141,7 +171,13 @@ def test_post_employees_201_response_invalid_json(openapi_version):
     with open("tests/invalid.json") as f:
         response = f.read()
     with pytest.raises(ValueError) as excinfo:
-        build_openapi("POST", "/employees", "201", response=response, openapi_version=openapi_version)
+        build_openapi(
+            "POST",
+            "/employees",
+            "201",
+            response=response,
+            openapi_version=openapi_version,
+        )
     assert "response" in str(excinfo)
 
 
@@ -149,5 +185,11 @@ def test_post_employees_201_response_invalid_yaml(openapi_version):
     with open("tests/invalid.yaml") as f:
         response = f.read()
     with pytest.raises(ValueError) as excinfo:
-        build_openapi("POST", "/employees", "201", response=response, openapi_version=openapi_version)
+        build_openapi(
+            "POST",
+            "/employees",
+            "201",
+            response=response,
+            openapi_version=openapi_version,
+        )
     assert "response" in str(excinfo)
